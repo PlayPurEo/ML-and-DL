24fa3d1124e6e7d759034614a6e68fce4a607554
Zhong Wang <542216917@qq.com>
2020-09-09 18:34:52 +0800

    first

---
+++ linear regression/linearCode.py
@@ -0,0 +1,8 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path = 'ex1data1.txt'
+data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
+print(data)
